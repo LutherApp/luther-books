883b1c84b88a63525919fc67845021ebc0324ac3
Dr. Martin Luther <jesu.broder@gmail.com>
2024-11-16 15:00:20 +0800

    build(table): add toggle rows

--- app/composables/states.ts
+++ app/composables/states.ts
@@ -1,2 +1,4 @@
 // export const useClickElement = () => useState<boolean>('clickElement', () => false)
 export const useClickElement = () => useState<string>('clickElement', () => '')
+
+export const useCloseElement = () => useState<HTMLButtonElement>('closeElement', () => null)
